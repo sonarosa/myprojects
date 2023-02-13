f0f7e9f323205f056376a55eef5b1c4e8861794f
sonarosa <sonarosababloo@gmail.com>
2023-02-13 09:11:45 +0530

    commit

--- my-portfolio/src/App.js
+++ my-portfolio/src/App.js
@@ -1,10 +1,20 @@
 import React from "react";
-import "./index.css"
+import "./index.css";
+import home from "./routes/home"
+import about from "./routes/about"
+import contact from "./routes/contact"
+import project from "./routes/project"
+import { Route, Routes } from "react-router-dom";
 function App() {
   return (
-    <div className="App">
-
-    </div>
+    <>
+      <Routes>
+        <Route path="/" element={<home/>}/>
+        <Route path="/project" element={<project/>}/>
+        <Route path="/about" element={<about/>}/>
+        <Route path="/contact" element={<contact/>}/>
+      </Routes>
+    </>
   );
 }
 
---
+++ my-portfolio/src/routes/contact.js
@@ -0,0 +1,5 @@
+import React from 'react'
+const contact=()=>{
+ return <div>Contact</div>;
+};
+export default contact;
